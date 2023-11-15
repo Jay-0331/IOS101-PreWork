a1e25a55acb819b4f8c38e6620c0323de84ef442
jay vekariya <vekariya.ja@northeastern.edu>
2023-11-14 21:08:09 -0500

    Added Labels and Button

--- IOS101-PreWork/ViewController.swift
+++ IOS101-PreWork/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let (red, green, blue) = changeColors()
+        changeUniColor?.textColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        changeNameColor?.textColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        changeJobColor?.textColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        changeButtonColor.tintColor = UIColor(red: red, green: green, blue: blue, alpha: 0.2)
+        changeButtonColor.configuration?.baseForegroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 0.3)
+    }
+    
+    @IBOutlet weak var changeButtonColor: UIButton!
+    @IBOutlet weak var changeNameColor: UILabel!
+    @IBOutlet weak var changeUniColor: UILabel!
+    @IBOutlet weak var changeJobColor: UILabel!
+    
+    func changeColors() -> (CGFloat, CGFloat, CGFloat) {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return (red, green, blue)
+    }
 
 }
 
